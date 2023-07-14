0b8cd4ed6b1052d510616fd8e54f520b02a463cf
raviraj3214 <raviraj32140@gmail.com>
2023-07-14 23:55:02 +0530

    15th commit

--- config/mongoose.js
+++ config/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 // mongoose.connect(`mongodb+srv://${process.env.username}:${process.env.password}@cluster0.zygzz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`);
 
-mongoose.connect('mongodb://127.0.0.1:27017/PlacementCell');
+mongoose.connect( process.env.DB);
 
 const db = mongoose.connection;
 
--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 9000;
